cc4dfec2ffb40e1796b121d00c0bb5597eeee5fd
jonathan <chevalier.jonathan@live.fr>
2019-07-16 13:23:36 -0300

    modified start script

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
 export dir="$(cd "$(dirname "$0")" && pwd)"
+dataDir=$dir/data
+
+if [ ! -d $dataDir ]; then
+  mkdir $dataDir
+fi
 
 docker-compose -f $dir/docker-compose.yaml up -d
